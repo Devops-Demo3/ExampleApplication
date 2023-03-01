6ec6ce0db1684e8b08c2efbd9c1b931a7dc1bd98
bregor77 <bregor77@gmail.com>
2023-03-01 19:11:18 +0000

    edit

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ sudo npm install -g pm2
 pm2 stop example_app
 cd ExampleApplication/
 npm install
-# sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-pm2 start ./bin/www --name example_app --env=production
-# pm2 start ./bin/www --name example_app
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+# pm2 start ./bin/www --name example_app --env=production
+pm2 start ./bin/www --name example_app
