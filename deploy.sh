2d2e592a2d3d0e1c4fdf7a8d9d812750c5391124
bregor77 <bregor77@gmail.com>
2023-03-01 16:05:27 +0000

    edit config.yml

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #./usr/binenv bash
 sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
-mp2 stop example_app
+pm2 stop example_app
 cd ExampleApplication/
 npm install
 # sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
